d7314a7d4dade156519a0a6906e115423649c8bd
Gino Sarubbi <110644113+SarubbiGino@users.noreply.github.com>
2023-02-16 17:52:53 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import Header from "../src/components/header/header";
 import FirstMain from "../src/components/FirstMain/FirstMain";
 import Footer from "../src/components/footer/footer";
-import Seccion from "./components/seccionGorras/seccionGorras";
-import Prueba from "./components/seccionRemeras/seccionRemeras";
+import SeccionGorras from "./components/seccionGorras/seccionGorras";
+import SeccionRemeras from "./components/seccionRemeras/seccionRemeras";
 import './App.css';
 
 
@@ -13,9 +13,8 @@ function App() {
     <div className="App bg-[#20433f] ">
       <Header />
       <FirstMain />
-      <Seccion />
-
-      <Prueba />
+      <SeccionGorras />
+      <SeccionRemeras />
       <Footer />
     </div>
   );
